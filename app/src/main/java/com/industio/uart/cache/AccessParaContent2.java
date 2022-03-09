8a4430fc915867f8699ae90d1b85219f988f0fac
lyle <12637011@qq.com>
2022-03-09 16:55:45 +0800

    修改通道参数

--- app/src/main/java/com/industio/uart/cache/AccessParaContent2.java
+++ app/src/main/java/com/industio/uart/cache/AccessParaContent2.java
@@ -3,11 +3,11 @@ package com.industio.uart.cache;
 import com.industio.uart.bean.AccessPara;
 
 public class AccessParaContent2 extends AccessParaContent {
-    private AccessPara accessPara1 = new AccessPara("通道1", "/dev/ttyS5", "/sys/class/leds/relay1/brightness");
-    private AccessPara accessPara2 = new AccessPara("通道2", "/dev/ttyS5", "/sys/class/leds/relay2/brightness");
-    private AccessPara accessPara3 = new AccessPara("通道3", "/dev/ttyS5", "/sys/class/leds/mos1/brightness");
-    private AccessPara accessPara4 = new AccessPara("通道4", "/dev/ttyS5", "/sys/class/leds/mos2/brightness");
-    private AccessPara accessPara5 = new AccessPara("通道5", "/dev/ttyS5", "/sys/class/leds/mos3/brightness");
+    private AccessPara accessPara1 = new AccessPara("通道1", "/dev/ttyS5", "/sys/class/leds/mos1/brightness");
+    private AccessPara accessPara2 = new AccessPara("通道2", "/dev/ttyS5", "/sys/class/leds/mos2/brightness");
+    private AccessPara accessPara3 = new AccessPara("通道3", "/dev/ttyS5", "/sys/class/leds/mos3/brightness");
+    private AccessPara accessPara4 = new AccessPara("通道4", "/dev/ttyS5", "/sys/class/leds/relay1/brightness");
+    private AccessPara accessPara5 = new AccessPara("通道5", "/dev/ttyS5", "/sys/class/leds/relay2/brightness");
 
     @Override
     public AccessPara accessPara1() {
