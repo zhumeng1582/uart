b56c5cf249b8a502a2caca36ca8e3aa46b81210f
zhangchao <zhumeng1582@163.com>
2022-03-11 15:36:51 +0800

    按钮

--- app/src/main/java/com/industio/uart/ui/LogDataFragment.java
+++ app/src/main/java/com/industio/uart/ui/LogDataFragment.java
@@ -66,7 +66,7 @@ public class LogDataFragment extends Fragment {
         binding.textCmdInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
                     sendCmd();
                     KeyboardUtils.hideSoftInput(getActivity());
                     return true;
