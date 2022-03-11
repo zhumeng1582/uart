817939b3ddeabfbe0ea57875e73986d96b6f2112
lyle <12637011@qq.com>
2022-03-11 13:11:03 +0800

    修改Log保存的目录为logs

--- app/src/main/java/com/industio/uart/utils/LogFileUtils.java
+++ app/src/main/java/com/industio/uart/utils/LogFileUtils.java
@@ -18,7 +18,7 @@ public class LogFileUtils {
             filePath = PathUtils.getDataPath();
         }
 
-        filePath = filePath + "/log";
+        filePath = filePath + "/logs";
 
 
         if (FileUtils.createOrExistsDir(filePath)) {
